2515606104d777e1763df257ec8f2ca8e31b4b37
Equinoxx01 <jamessarucam517@gmail.com>
2025-07-24 13:58:03 +0800

    First Push

---
+++ src/sarucam/Sarucam.java
@@ -0,0 +1,14 @@
+
+package sarucam;
+
+
+public class Sarucam {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hello Word!");
+        
+    }
+    
+}
